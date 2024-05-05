7bf6eb33cfb94426bf000967e348b4ec9b9d5ccd
Tsvetoslav Vasilev <tsvetivasilev@gmail.com>
2024-05-05 19:45:34 +0200

    Junky coloring

--- resizable_grid_init.c
+++ resizable_grid_init.c
@@ -1,76 +1,68 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 #define BOARD_SIZE 4
 
-#include <stdio.h>
+#define COLOR_TWO 8
+#define COLOR_FOUR 9
+#define COLOR_EIGHT 10
+#define COLOR_SIXTEEN 11
+#define COLOR_THIRTY2 12
+#define COLOR_SIXTY4 13
+#define COLOR_ONE28 14
+#define COLOR_TWO56 15
+#define COLOR_FIVE12 16
+#define COLOR_TEN24 17
+#define COLOR_TWENTY48 18
+#define COLOR_EMPTY 19
 
-// char ascii_art[][11] = {
-//     " ██████ ",
-//     "██    ██",
-//     "██    ██",
-//     "██    ██",
-//     " ██████ ",
+// Function to convert hex color to RGB
+void hexToRGB(const char* hexColor, int* r, int* g, int* b) {
+    *r = 0; *g = 0; *b = 0; 
+    sscanf(hexColor, "#%02x%02x%02x", r, g, b);
+}
 
-//     "    ██",
-//     "   ███",
-//     "    ██",
-//     "    ██",
-//     "    ██",
-    
-//     "██████ ",
-//     "     ██",
-//     " █████ ",
-//     "██     ",
-//     "███████",
-
-//     "██████ ",
-//     "     ██",
-//     "█████ ",
-//     "     ██",
-//     "██████ ",
-
-//     "██   ██",
-//     "██   ██",
-//     "███████",
-//     "     ██",
-//     "     ██",
-
-//     "███████",
-//     "██     ",
-//     "███████",
-//     "     ██",
-//     "███████",
-
-//     " ██████",
-//     "██     ",
-//     "███████ ",
-//     "██    ██",
-//     " ██████ ",
-
-//     "███████",
-//     "     ██",
-//     "    ██ ",
-//     "   ██  ",
-//     "   ██  ",
-
-//     " █████ ",
-//     "██   ██",
-//     " █████ ",
-//     "██   ██",
-//     " █████ ",
-
-//     " █████ ",
-//     "██   ██",
-//     " █████ ",
-//     "    ██ ",
-//     " █████ "
-
-// };
-
-
-
+void init_colors() {
+    int r, g, b;
+    hexToRGB("#cdc1b4", &r, &g, &b);
+    init_color(COLOR_EMPTY, r*3*3, g*3*3, b*3*3);
+    init_pair(COLOR_EMPTY, COLOR_EMPTY, COLOR_EMPTY);
+    hexToRGB("#eee4da", &r, &g, &b);
+    init_color(COLOR_TWO, r*3*3, g*3*3, b*3*3);
+    init_pair(COLOR_TWO, COLOR_TWO, COLOR_TWO);
+    hexToRGB("#ede0c8", &r, &g, &b);
+    init_color(COLOR_FOUR, r*3*3, g*3*3, b*3*3);
+    init_pair(COLOR_FOUR, COLOR_FOUR, COLOR_FOUR);
+    hexToRGB("#f2b179", &r, &g, &b);
+    init_color(COLOR_EIGHT, r*3, g*3, b*3);
+    init_pair(COLOR_EIGHT, COLOR_EIGHT, COLOR_EIGHT);
+    hexToRGB("#f59563", &r, &g, &b);
+    init_color(COLOR_SIXTEEN, r*3, g*3, b*3);
+    init_pair(COLOR_SIXTEEN, COLOR_SIXTEEN, COLOR_SIXTEEN);
+    hexToRGB("#f67c5f", &r, &g, &b);
+    init_color(COLOR_THIRTY2, r*3, g*3, b*3);
+    init_pair(COLOR_THIRTY2, COLOR_THIRTY2, COLOR_THIRTY2);
+    hexToRGB("#f65e3b", &r, &g, &b);
+    init_color(COLOR_SIXTY4, r*3, g*3, b*3);
+    init_pair(COLOR_SIXTY4, COLOR_SIXTY4, COLOR_SIXTY4);
+    hexToRGB("#edcf72", &r, &g, &b);
+    init_color(COLOR_ONE28, r*3, g*3, b*3);
+    init_pair(COLOR_ONE28, COLOR_ONE28, COLOR_ONE28);
+    hexToRGB("#edcc61", &r, &g, &b);
+    init_color(COLOR_TWO56, r*3, g*3, b*3);
+    init_pair(COLOR_TWO56, COLOR_TWO56, COLOR_TWO56);
+    hexToRGB("#edc850", &r, &g, &b);
+    init_color(COLOR_FIVE12, r*3, g*3, b*3);
+    init_pair(COLOR_FIVE12, COLOR_FIVE12, COLOR_FIVE12);
+    hexToRGB("#edc53f", &r, &g, &b);
+    init_color(COLOR_TEN24, r*3, g*3, b*3);
+    init_pair(COLOR_TEN24, COLOR_TEN24, COLOR_TEN24);
+    hexToRGB("#edc22e", &r, &g, &b);
+    init_color(COLOR_TWENTY48, r*3, g*3, b*3);
+    init_pair(COLOR_TWENTY48, COLOR_TWENTY48, COLOR_TWENTY48);
+}
 char ascii_art[][11] = {
     " 0000",
     "00  00",
@@ -235,6 +227,8 @@ void draw_board(int board[][BOARD_SIZE]) {
                 int next_y = cell_height / 2 + (i + 1) * cell_height;
                 mvvline(y + 1, x, '|', next_y - y - 1);
             }
+
+            // Draw the number in the cell
             if (i < BOARD_SIZE && j < BOARD_SIZE){
                 int size = 5;
                 int temp = board[i][j];
@@ -248,7 +242,11 @@ void draw_board(int board[][BOARD_SIZE]) {
                         y_offset = 0;
                         for (int k = 0; k < size; k++) {
                                 if (board[i][j] != 0){
-                                    mvprintw(y + cell_height / 2 - size / 2 + y_offset , x + x_offset-size/2 + cell_width/2 + (count*(size + 2))/2 - size/2, "%s", ascii_art[(temp % 10) * size + k]);
+                                    attron(COLOR_PAIR(COLOR_FIVE12));
+                                    mvprintw(y + cell_height / 2 - size / 2 + y_offset ,
+                                        x + x_offset-size/2 + cell_width/2 + (count*(size + 2))/2 - size/2,
+                                            "%s", ascii_art[(temp % 10) * size + k]);
+                                    attroff(COLOR_PAIR(COLOR_FIVE12));
                                 }
                                 y_offset += 1;
                         }
@@ -258,16 +256,18 @@ void draw_board(int board[][BOARD_SIZE]) {
                 }
             }
         }       
-                //increase y value
     }
-    // Refresh the screen to show the updated grid
     refresh();
 }
 
 int main() {
     initscr();
+    start_color();
+    init_colors();
     raw();
     noecho();
+
+    
 
     int board[BOARD_SIZE][BOARD_SIZE] = {10, 10, 10, 100, 32, 64, 128, 256, 512, 1024, 2048, 65536, 8192, 16384, 32768, 4096};
     //  int board[BOARD_SIZE][BOARD_SIZE] = {2048};
